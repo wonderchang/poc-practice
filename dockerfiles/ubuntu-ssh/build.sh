f1e909a98a167e84a70125a151e99d6d50111a92
Wonder Chang <wonderchang@kkbox.com>
2017-05-15 20:29:06 +0800

    Almost done user setup subjects

---
+++ dockerfiles/ubuntu-ssh/build.sh
@@ -0,0 +1,3 @@
+docker build -t 'wonderchang/poc-practice-ubuntu-ssh' .
+sleep 3
+docker push wonderchang/poc-practice-ubuntu-ssh
---
+++ subjects/user-setup/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+HOSTNAME="server"
+USERNAME="ubuntu"
+PASSWORD="ubuntu"
+
+# Build container
+if [ "$(/usr/local/bin/docker-machine ls | grep "^$HOSTNAME " | cut -d' ' -f1)" == "$HOSTNAME" ]; then
+  echo "Host $HOSTNAME already exists"
+  /usr/local/bin/docker-machine rm $HOSTNAME
+fi
+
+/usr/local/bin/docker-machine create --driver virtualbox $HOSTNAME
+eval $(docker-machine env $HOSTNAME)
+/usr/local/bin/docker-compose up --build -d
+
+# Deploy the user initialization
+ip=$(/usr/local/bin/docker-machine ls | grep "^$HOSTNAME " | awk '{print $5}' | grep -o "192\.168\.[0-9]*\.[0-9]*")
+echo "server ansible_ssh_host=$ip ansible_ssh_port=2222 ansible_ssh_user=$USERNAME ansible_ssh_pass=$PASSWORD ansible_become_pass=$PASSWORD" > inventory
+/usr/local/bin/ansible-playbook -i inventory playbook.yml
